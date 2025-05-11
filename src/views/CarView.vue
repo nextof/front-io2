617362f4162e13ec33d18c774e5160f08b51eeda
nextof <kwojcik.light@gmail.com>
2025-05-11 16:28:14 +0200

    Update CarView.vue

--- src/views/CarView.vue
+++ src/views/CarView.vue
@@ -225,38 +225,7 @@ onMounted(async () => {
             />
           </div>
           
-          <div>
-            <label for="clientName" class="block text-sm font-medium text-gray-700 mb-1">Full Name</label>
-            <input 
-              type="text" 
-              id="clientName" 
-              v-model="reservationData.clientName" 
-              class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
-              required
-            />
-          </div>
-          
-          <div>
-            <label for="clientEmail" class="block text-sm font-medium text-gray-700 mb-1">Email Address</label>
-            <input 
-              type="email" 
-              id="clientEmail" 
-              v-model="reservationData.clientEmail" 
-              class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
-              required
-            />
-          </div>
-          
-          <div class="md:col-span-2">
-            <label for="clientPhone" class="block text-sm font-medium text-gray-700 mb-1">Phone Number</label>
-            <input 
-              type="tel" 
-              id="clientPhone" 
-              v-model="reservationData.clientPhone" 
-              class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
-              required
-            />
-          </div>
+
           
           <div class="md:col-span-2 flex justify-end space-x-4">
             <button 

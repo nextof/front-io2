294aab6a820cd625d06ac6d9478fcbde64226bf0
nextof <kwojcik.light@gmail.com>
2025-05-13 12:22:55 +0200

    delete vehicle

--- src/views/ManageVehiclesView.vue
+++ src/views/ManageVehiclesView.vue
@@ -133,30 +133,40 @@
                     {{ vehicle.status }}
                   </span>
                 </td>
-                <td class="py-3 px-4">
-                  <div class="flex space-x-2">
-                    <button
-  @click="openEditVehicleModal(vehicle)"
-  class="text-blue-600 hover:text-blue-800 cursor-pointer transition-colors duration-300 transform hover:scale-110"
-  title="Edit"
->
-  <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path>
-  </svg>
-</button>
+<td class="py-3 px-4">
+  <div class="flex space-x-2">
+    <button
+      @click="openEditVehicleModal(vehicle)"
+      class="text-blue-600 hover:text-blue-800 cursor-pointer transition-colors duration-300 transform hover:scale-110"
+      title="Edit"
+    >
+      <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path>
+      </svg>
+    </button>
 
-<RouterLink
-  :to="`/vehicles/${vehicle.id}`"
-  class="text-green-600 hover:text-green-800 transition-colors duration-300 transform hover:scale-110"
-  title="View Details"
->
-  <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
-    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"></path>
-  </svg>
-</RouterLink>
-                  </div>
-                </td>
+    <RouterLink
+      :to="`/vehicles/${vehicle.id}`"
+      class="text-green-600 hover:text-green-800 transition-colors duration-300 transform hover:scale-110"
+      title="View Details"
+    >
+      <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"></path>
+      </svg>
+    </RouterLink>
+
+    <button
+      @click="deleteVehicle(vehicle)"
+      class="text-red-600 hover:text-red-800 cursor-pointer transition-colors duration-300 transform hover:scale-110"
+      title="Delete"
+    >
+      <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path>
+      </svg>
+    </button>
+  </div>
+</td>
               </tr>
               <!-- Empty state -->
               <tr v-if="filteredVehicles.length === 0">
@@ -628,7 +638,21 @@ export default {
     const closeVehicleModal = () => {
       showVehicleModal.value = false;
     };
-
+// Delete vehicle
+const deleteVehicle = async (vehicle) => {
+  if (confirm(`Are you sure you want to delete "${vehicle.make} ${vehicle.model}" (${vehicle.license_plate})?`)) {
+    try {
+      await VehicleService.deleteVehicle(vehicle.id);
+      alert('Vehicle deleted successfully!');
+      
+      // Refresh the vehicle list
+      await fetchVehicles();
+    } catch (err) {
+      console.error('Error deleting vehicle:', err);
+      alert('Failed to delete vehicle. Please try again.');
+    }
+  }
+};
     // Save vehicle (create or update)
     const saveVehicle = async () => {
       isSaving.value = true;
@@ -677,27 +701,27 @@ export default {
     onMounted(() => {
       fetchVehicles();
     });
-
-    return {
-      vehicles,
-      loading,
-      error,
-      searchQuery,
-      filteredVehicles,
-      showVehicleModal,
-      isSaving,
-      isEditMode,
-      vehicleForm,
-      sortField,
-      sortDirection,
-      toggleSort,
-      openAddVehicleModal,
-      openEditVehicleModal,
-      closeVehicleModal,
-      saveVehicle,
-      addFeature,
-      removeFeature
-    };
+return {
+  vehicles,
+  loading,
+  error,
+  searchQuery,
+  filteredVehicles,
+  showVehicleModal,
+  isSaving,
+  isEditMode,
+  vehicleForm,
+  sortField,
+  sortDirection,
+  toggleSort,
+  openAddVehicleModal,
+  openEditVehicleModal,
+  closeVehicleModal,
+  saveVehicle,
+  deleteVehicle,  // <-- Add this line
+  addFeature,
+  removeFeature
+};
   }
 };
 </script>
